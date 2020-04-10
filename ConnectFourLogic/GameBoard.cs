b8f2aec991abebc30d5a67ba3090b12b9af7f2ea
tk-codes <3751899+tk-codes@users.noreply.github.com>
2020-04-10 22:35:55 +0200

    feat: create board cell view

--- ConnectFourLogic/GameBoard.cs
+++ ConnectFourLogic/GameBoard.cs
@@ -6,6 +6,6 @@ namespace ConnectFourLogic
 {
     public class GameBoard
     {
-        public int[,] Cells { get; } = new int[6, 7];
+        public Player[,] Cells { get; } = new Player[6, 7];
     }
 }
