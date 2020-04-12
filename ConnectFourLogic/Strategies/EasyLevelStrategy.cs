2e30b230f27fa0ae68b59e0aab4e3385c3580e6d
tk-codes <3751899+tk-codes@users.noreply.github.com>
2020-04-12 12:47:39 +0200

    feat: create game strategy factory

---
+++ ConnectFourLogic/Strategies/EasyLevelStrategy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConnectFourLogic.Strategies
+{
+    class EasyLevelStrategy : IGameStrategy
+    {
+
+    }
+}
---
+++ ConnectFourLogic/Strategies/GameStrategyFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace ConnectFourLogic.Strategies
+{
+    class GameStrategyFactory
+    {
+        public static IGameStrategy Create(GameStrategyLevel level)
+        {
+            return level switch
+            {
+                GameStrategyLevel.Easy => new EasyLevelStrategy(),
+                _ => throw new NotSupportedException($"Strategy {level} is not supported yet")
+            };
+        }
+    }
+}
