f66a6d96543d48c2f0c449e61ceca4a38bec9fa3
tk-codes <3751899+tk-codes@users.noreply.github.com>
2020-04-12 22:21:20 +0200

    refactor: implement generic CanConnectFour method

---
+++ ConnectFourLogic.Test/BoardCellTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace ConnectFourLogic.Test
+{
+    public class BoardCellTest
+    {
+        [Fact]
+        public void ShouldBeEqual()
+        {
+            var cell1 = new BoardCell(0, 0);
+            var cell2 = new BoardCell(0, 0);
+
+            var result = cell1.Equals(cell2);
+
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ShouldNotBeEqual()
+        {
+            var cell1 = new BoardCell(0, 1);
+            var cell2 = new BoardCell(0, 0);
+
+            var result = cell1.Equals(cell2);
+
+            result.Should().BeFalse();
+        }
+    }
+}
