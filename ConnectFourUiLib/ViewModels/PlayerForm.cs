c670ed8c3904a84d292579b13254769537fbaac8
tk-codes <3751899+tk-codes@users.noreply.github.com>
2022-11-09 15:17:14 +0100

    feat: Create Razor components library for player form

---
+++ ConnectFourUiLib/ViewModels/PlayerForm.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConnectFourUiLib.ViewModels
+{
+    public class PlayerForm
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "Name is too short")]
+        public string Name { get; set; }
+
+        [Required]
+        [RegularExpression("^#[A-Fa-f0-9]{6}$", ErrorMessage = "Invalid Color")]
+        public string Color { get; set; }
+    }
+}
